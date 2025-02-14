5a2c89738c41da735124e8ca2bfd25b218f4faa0
Mỹ Xoan Trần Thị <xoanxoan2501>
2025-02-14 14:53:43 +0700

    homepage30%

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,21 +1,22 @@
-import { Card } from 'antd'
 import './Homepage.scss'
 import { useAppSelector } from '@/shared/hook/reduxHooks'
+import MySlider from '../Auth/component/slider/slider'
+import { Container } from '@mui/material'
+import BestSeller from '../Auth/component/bestSeller/bestSeller'
+import ContractHome from '../Auth/component/ContractHome/ContractHome'
 
 function Home() {
   const userLogin = useAppSelector((state) => state.profile.user)
 
   return (
     <div className="home__container">
-      <Card title="Home" className="home__card">
-        <h1>Home page</h1>
-        <p>Chào mừng bạn đến với trang Home!</p>
-        <p>
-          {userLogin
-            ? `Xin chào ${userLogin?.displayName}`
-            : 'Bạn chưa đăng nhập'}
-        </p>
-      </Card>
+      <Container>
+        <MySlider />
+        <div className="line_home"> </div>
+        <BestSeller />
+        <div className="line_home"> </div>
+        <ContractHome />
+      </Container>
     </div>
   )
 }
