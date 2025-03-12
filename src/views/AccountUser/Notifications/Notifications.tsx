cdfd63e421d533aa53d5c6c7d3684e3afd01510c
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-03-12 10:25:36 +0700

    code fe notification and info-payment

--- src/views/AccountUser/Notifications/Notifications.tsx
+++ src/views/AccountUser/Notifications/Notifications.tsx
@@ -1,7 +1,98 @@
 import { LayoutBox } from '../../Profile/components/LayoutBox'
+import { Box, Button, Paper, Tab, Tabs, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { routerDetailPurchase } from '../purchase/page/router'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+import CancelIcon from '@mui/icons-material/Cancel'
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"
+import LocalOfferIcon from "@mui/icons-material/LocalOffer"
+import FireIcon from "@mui/icons-material/Whatshot"
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
+import React, { ReactElement, useState } from 'react'
+import './notifications.css'
+
+const notifications = [
+  {
+    icon: <CheckCircleIcon sx={{ color: "#3b82f6", fontSize: 40 }} />,
+    title: "Giao kiện hàng thành công",
+    description: "Kiện hàng SPW001 đã được giao thành công tới bạn.",
+    type: "order"
+  },
+  {
+    icon: <LocalShippingIcon sx={{ color: "#c084fc", fontSize: 40 }} />,
+    title: "Xác nhận đơn hàng",
+    description: "Kiện hàng SPW001 đã được xác nhận.",
+    type: "order"
+  },
+  {
+    icon: <LocalOfferIcon sx={{ color: "#ec4899", fontSize: 40 }} />,
+    title: "SIÊU HOT! MÃ GIẢM 200K SẴN SÀNG",
+    description: (
+      <>
+        <Typography>🔥 Thêm mã giảm 200k chỉ có hôm nay!</Typography>
+        <Typography>🛒 Freeship tận nơi - mua sắm ngay</Typography>
+      </>
+    ),
+    type: "voucher"
+  }
+]
 
 function Notifications() {
-  return <LayoutBox></LayoutBox>
+  const [selectedTab, setSelectedTab] = useState(0) // 0: Tất cả, 1: Đơn hàng, 2: Mã giảm giá
+
+  // Hàm xử lý khi đổi tab
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setSelectedTab(newValue)
+  }
+
+  // Lọc thông báo theo tab được chọn
+  const filteredNotifications = notifications.filter((item) => {
+    if (selectedTab === 0) return true // Tất cả
+    if (selectedTab === 1) return item.type === "order"// Chỉ đơn hàng
+    if (selectedTab === 2) return item.type === "voucher" // Chỉ mã giảm giá
+    return false
+  })
+
+  return (
+    <LayoutBox sx={{ background: "#f8cfd3" }}>
+      <Box sx={{ backgroundColor: "#f8cfd3", p: 3, borderRadius: 2, maxWidth: "100%", mx: "auto", mt: 4, padding: 1 }}>
+        <Typography variant="h6" sx={{ fontWeight: "bold", pb: 2, textAlign: "center", fontSize: "30px" }}>
+          Thông báo
+        </Typography>
+
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
+          <Tabs value={selectedTab} onChange={handleChange} textColor="secondary" indicatorColor="secondary">
+            <Tab label="Tất cả" sx={{ fontWeight: "bold", color: "#000", fontSize: "20px" }} />
+            <Tab label="Đơn hàng" sx={{ fontWeight: "bold", color: "#000", fontSize: "20px" }}/>
+            <Tab label="Mã giảm giá" sx={{ fontWeight: "bold", color: "#000", fontSize: "20px" }}/>
+          </Tabs>
+        </Box>
+
+        <Box sx={{ mt: 3 }}>
+          {filteredNotifications.map((item, index) => (
+            <Box key={index} sx={{ display: "flex", alignItems: "center", p: 2, backgroundColor: "#fff", borderRadius: 2, mb: 3, border: "1px solid #f1a4b5", padding: "30px" }}>
+              <Box sx={{ mr: 2 }}>{item.icon}</Box>
+              <Box sx={{ flexGrow: 1 }}>
+                <Typography sx={{ fontWeight: "bold", fontSize: "30px" }}>{item.title}</Typography>
+                {typeof item.description === "string" ? (
+                  <Typography variant="body2" sx={{ fontSize: "20px" }}>{item.description}</Typography>
+                ) : (
+                  React.Children.map(item.description.props.children, (child, i) => (
+                    <div className='typograph-des'>
+                      <Typography key={i} variant="body2" sx={{ fontSize: "20px" }}>
+                        {child}
+                      </Typography>
+                    </div>
+                  ))
+                )}
+              </Box>
+              <Typography sx={{ fontSize: "20px", fontWeight: "bold", color: "#000" }}>Xem chi tiết</Typography>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </LayoutBox>
+  )
 }
 
 export default Notifications
--- src/views/AccountUser/PaymentUser/PaymentUser.tsx
+++ src/views/AccountUser/PaymentUser/PaymentUser.tsx
@@ -1,7 +1,77 @@
+import { Box, Button, Typography } from '@mui/material'
 import { LayoutBox } from '../../Profile/components/LayoutBox'
-
+import './paymentUser.css'
 function PaymentUser() {
-  return <LayoutBox> thong tin thanh toan</LayoutBox>
+  return (
+    <LayoutBox>
+      <Box sx={{ background: "#F2C2CF", padding: "15px", fontWeight: "bold", fontSize: "25px", borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}>
+        Thông tin thanh toán:
+      </Box>
+
+      <Box sx={{ fontSize: "20px" }}>
+
+        <Box sx={{ padding: "15px", borderBottom: "1px solid #ddd", backgroundColor: "#fff" }}>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography sx={{ fontWeight: "bold", fontSize: "16px", marginBottom: "10px" }}>Thẻ tín dụng / ghi nợ</Typography>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#F2C2CF", color: "black", textTransform: "none", fontSize: "20px", borderRadius: "15px", padding: "0px 10px 0px 10px", marginRight: "30px", fontWeight: "500" }}
+            >
+              + Liên kết thẻ
+            </Button>
+          </Box>
+
+          <hr style={{ border: "2px solid #000" }}></hr>
+
+          <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "10px", padding: "30px", fontSize: "40px !important" }}>
+            <Typography>Bạn chưa liên kết thẻ</Typography>
+          </Box>
+        </Box>
+
+        <Box sx={{ marginTop: "40px", backgroundColor: "#fff", fontSize: "20px" }}>
+
+          <Box sx={{ padding: "15px", fontWeight: "bold" }}>Tài khoản ngân hàng của tôi</Box>
+          <hr style={{ border: "2px solid #000", marginLeft: "15px", marginRight: "20px" }}></hr>
+
+          <Box sx={{ padding: "5px", marginLeft: "45px", fontSize: "15px" }}>
+            <Typography>Ngân hàng : <strong>SACOMBANK</strong></Typography>
+            <Typography>Họ tên chủ tài khoản : <strong>TRAN THI MY XOAN</strong></Typography>
+            <Typography>Số tài khoản : <strong>012345678910</strong></Typography>
+          </Box>
+
+        </Box>
+
+      </Box>
+      {/*
+
+      <Box sx={{ maxWidth: "600px", margin: "auto", borderRadius: "10px", overflow: "hidden", background: "#f8f8f8" }}>
+        <Box sx={{ background: "#F2C2CF", padding: "15px", fontWeight: "bold", fontSize: "18px", borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}>
+          Thông tin thanh toán:
+        </Box>
+        <Box sx={{ padding: "20px", background: "white" }}>
+          <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <Typography sx={{ fontWeight: "bold", fontSize: "16px" }}>Thẻ tín dụng / ghi nợ</Typography>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#F2C2CF", color: "black", textTransform: "none", fontSize: "14px", borderRadius: "20px", padding: "5px 15px", minWidth: "unset", boxShadow: "none", border: "1px solid #D69AA5" }}
+            >
+              + Liên kết thẻ
+            </Button>
+          </Box>
+          <Box sx={{ borderBottom: "1px solid #777", marginTop: "5px" }} />
+          <Box sx={{ textAlign: "center", marginTop: "20px", color: "#000", fontSize: "16px", fontWeight: "bold" }}>
+            Bạn chưa liên kết thẻ
+          </Box>
+        </Box>
+        <Box sx={{ background: "#F2C2CF", padding: "15px", fontWeight: "bold", fontSize: "16px" }}>Tài khoản ngân hàng của tôi</Box>
+        <Box sx={{ padding: "20px", background: "white", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px" }}>
+          <Typography sx={{ fontSize: "14px", marginBottom: "5px" }}>Ngân hàng : <strong>SACOMBANK</strong></Typography>
+          <Typography sx={{ fontSize: "14px", marginBottom: "5px" }}>Họ tên chủ tài khoản : <strong>TRAN THI MY XOAN</strong></Typography>
+          <Typography sx={{ fontSize: "14px" }}>Số tài khoản : <strong>012345678910</strong></Typography>
+        </Box>
+      </Box> */}
+    </LayoutBox>
+  )
 }
 
 export default PaymentUser
