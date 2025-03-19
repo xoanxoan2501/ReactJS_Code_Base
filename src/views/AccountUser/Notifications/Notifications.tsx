c10e8bae6def80e7c11122eff26b5dfcbc8da547
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-03-19 23:44:19 +0700

    update page notification

--- src/views/AccountUser/Notifications/Notifications.tsx
+++ src/views/AccountUser/Notifications/Notifications.tsx
@@ -6,49 +6,32 @@ import './notifications.css'
 import iconShip from '@/assets/icons/iconShip.png'
 import iconTick from '@/assets/icons/iconTick.png'
 import iconVoucher from '@/assets/icons/iconVoucher.png'
+import iconFail from '@/assets/icons/failIcon.png'
 
 const notifications = [
   {
-    icon: <img src={iconTick} style={{
-      color: "#3b82f6",
-      fontSize: 30,
-      padding: 15,
-      borderRadius: "50%",
-      width: 70,
-      height: 70,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#dbeafe"
-    }}/>,
+    icon: <img src={iconTick}/>,
     title: "Giao kiện hàng thành công",
     description: "Kiện hàng SPW001 đã được giao thành công tới bạn.",
-    type: "order"
+    type: "order",
+    status: "success"
   },
   {
-    icon: <img src={iconShip} style={{
-      color: "#3b82f6",
-      padding: 15,
-      borderRadius: "50%",
-      width: 70,
-      height: 70,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#dbeafe"
-    }}/>,
+    icon: <img src={iconFail}/>,
+    title: "Giao kiện hàng không thành công",
+    description: "Kiện hàng SPW001 không được giao thành công tới bạn.",
+    type: "order",
+    status: "fail"
+  },
+  {
+    icon: <img src={iconShip}/>,
     title: "Xác nhận đơn hàng",
     description: "Kiện hàng SPW001 đã được xác nhận.",
-    type: "order"
+    type: "order",
+    status: "shipping"
   },
   {
-    icon: <img src={iconVoucher} style={{
-      width: 70,
-      height: 70,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
-    }}/>,
+    icon: <img src={iconVoucher}/>,
     title: "SIÊU HOT! MÃ GIẢM 200K SẴN SÀNG",
     description: (
       <>
@@ -77,21 +60,23 @@ function Notifications() {
   })
 
   return (
-    <LayoutBox sx={{ background: "#f8cfd3" }}>
-      <Box sx={{ backgroundColor: "#f8cfd3", p: 3, borderRadius: 2, maxWidth: "100%", mx: "auto", mt: 4, padding: 1 }}>
-        <Typography variant="h6" sx={{ fontWeight: "bold", pb: 2, textAlign: "center", fontSize: "30px" }}>
+    <LayoutBox sx={{ background: "#F2C2CF" }}>
+      <Box sx={{ p: 3, borderRadius: 2, maxWidth: "100%", mx: "auto", mt: 4, padding: 1 }}>
+        <Typography variant="h6" sx={{ pb: 2, textAlign: "center", fontSize: "30px" }}>
           Thông báo
         </Typography>
 
         <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
           <Tabs value={selectedTab} onChange={handleChange} textColor="secondary" indicatorColor="secondary">
-            <Tab label="Tất cả" sx={{ fontWeight: "bold", color: "#000", fontSize: "20px" }} />
-            <Tab label="Đơn hàng" sx={{ fontWeight: "bold", color: "#000", fontSize: "20px" }}/>
-            <Tab label="Mã giảm giá" sx={{ fontWeight: "bold", color: "#000", fontSize: "20px" }}/>
+            <Tab label="Tất cả" sx={{ color: "#000", fontSize: "20px" }} />
+            <Tab label="Đơn hàng" sx={{ color: "#000", fontSize: "20px" }}/>
+            <Tab label="Mã giảm giá" sx={{ color: "#000", fontSize: "20px" }}/>
           </Tabs>
         </Box>
 
-        <Box sx={{ mt: 3, maxHeight: "470px", overflowY: "auto", pr: "5px",
+        <Box sx={{ mt: 3, maxHeight: "470px", overflowY: "auto", pr: "1px",
+          boxSizing: "border-box",
+          width: "100%",
           mr: "-8px",
           "&::-webkit-scrollbar": {
             width: "6px"
@@ -100,12 +85,21 @@ function Notifications() {
             background: "transparent",
           },
           "&::-webkit-scrollbar-thumb": {
-            background: "#d1d1d1",
+            background: "#8B8B8B",
             borderRadius: "6px"
           } }}>
           {filteredNotifications.map((item, index) => (
-            <Box key={index} sx={{ display: "flex", alignItems: "center", p: 2, backgroundColor: "#fff", borderRadius: 2, mb: 3, border: "1px solid #f1a4b5", padding: "30px" }}>
-              <Box sx={{ mr: 2 }}>{item.icon}</Box>
+            <Box key={index} sx={{ display: "flex", alignItems: "center", backgroundColor: "#fff", mb: 3, padding: "30px", height: "150px", width: "100%" }}>
+              <Box sx={{ mr: 2 }}>{item.status === "success" ? (
+                <img src={iconTick} style={{ width: "50px", height: "50px", objectFit: "contain" }}/>
+              ) : item.status === "fail" ? (
+                <img src={iconFail} style={{ width: "50px", height: "50px", objectFit: "contain" }} />
+              ) : item.status === "shipping" ? (
+                <img src={iconShip} style={{ width: "50px", height: "50px", objectFit: "contain" }}/>
+              ) : (
+                <img src={iconVoucher} style={{ width: "50px", height: "50px", objectFit: "contain" }}/>
+              )}
+              </Box>
               <Box sx={{ flexGrow: 1 }}>
                 <Typography sx={{ fontWeight: "bold", fontSize: "30px" }}>{item.title}</Typography>
                 {typeof item.description === "string" ? (
